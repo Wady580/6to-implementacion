59db873f13d4842d91842f4c103e4f77bbe8cfee
Azcona <2190003ipisa.edu.do>
2023-01-05 20:16:02 -0400

    Configuracion de la tabla de inventario

--- src/main/frm_inventario.java
+++ src/main/frm_inventario.java
@@ -5,17 +5,31 @@
  */
 package main;
 
+import connection.MyConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author A19B45923
  */
 public class frm_inventario extends javax.swing.JFrame {
+    
+      DefaultTableModel productos; 
 
     /**
      * Creates new form frm_inventario
      */
     public frm_inventario() {
         initComponents();
+        
+         this.productos = (DefaultTableModel) tbl_productos.getModel();
     }
 
     /**
@@ -27,32 +41,78 @@ public class frm_inventario extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        lbl_inventario = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbl_productos = new javax.swing.JTable();
+        txt_buscador = new javax.swing.JTextField();
+        btn_buscar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("Holaa");
+        lbl_inventario.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        lbl_inventario.setText("Inventario");
+
+        tbl_productos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(tbl_productos);
+
+        txt_buscador.setText("jTextField1");
+
+        btn_buscar.setText("Buscar");
+        btn_buscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_buscarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(159, 159, 159)
-                .addComponent(jLabel1)
-                .addContainerGap(181, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lbl_inventario, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 302, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txt_buscador, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btn_buscar)))))
+                .addContainerGap(315, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(113, 113, 113)
-                .addComponent(jLabel1)
-                .addContainerGap(158, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(lbl_inventario)
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txt_buscador, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_buscar))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(89, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_buscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_buscarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_buscarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -90,6 +150,80 @@ public class frm_inventario extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btn_buscar;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lbl_inventario;
+    private javax.swing.JTable tbl_productos;
+    private javax.swing.JTextField txt_buscador;
     // End of variables declaration//GEN-END:variables
+
+    public final void Mostrardatos(String valor){
+            //Funcion para llenar la jtable de Usuarios desde la BD
+            //Myconnection cc=new Myconnection();
+            Connection cn=MyConnection.getConnection();
+            RefrescarTabla();
+            productos.addColumn("idproducto");
+            productos.addColumn("nombre");
+            productos.addColumn("proveedor");
+            productos.addColumn("stock");
+            productos.addColumn("descripcion");
+            productos.addColumn("precio");
+            productos.addColumn("codigopro");
+            productos.addColumn("tipoproducto");
+            
+            
+            this.tbl_productos.setModel(productos);
+            String sql;
+            if (valor.equals(""))
+            {
+            sql="SELECT * FROM productos";
+            }
+            else{
+            sql="SELECT * FROM productos WHERE productos='"+valor+"'";
+            } 
+
+            String []datos=new String [5];
+            try{
+            Statement st=cn.createStatement();
+            ResultSet rs=st.executeQuery(sql);
+            while(rs.next()){
+            datos[0]=rs.getString(1);
+            datos[1]=rs.getString(2);
+            datos[2]=rs.getString(3);
+            datos[3]=rs.getString(4);
+            datos[4]=rs.getString(5);
+           
+            
+     
+
+            productos.addRow(datos);
+            }
+            tbl_productos.setModel(productos);
+            }catch(SQLException ex){
+            Logger.getLogger(frm_inventario.class.getName()).log(Level.SEVERE,null,ex);
+            JOptionPane.showMessageDialog(null, "error "+ex);
+            }
+        }
+
+    private void RefrescarTabla() {
+     
+           //Funcion encargada de refrescar la tabla utilizando revalidate
+        try {
+        productos.setColumnCount(0);
+        productos.setRowCount(0);
+        tbl_productos.revalidate();
+        }
+        
+        //Mensaje de error
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "error "+ex);
+        }
+   
+    }
+    
 }
+       
+    
+    
+         
+
